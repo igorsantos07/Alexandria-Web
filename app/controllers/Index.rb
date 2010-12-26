32102315b72262184390fa0b196c1cdf78a209bc
Igor Santos <igorsantos07@gmail.com>
2010-12-26 19:45:36 -0200

    Basic read and listing of YAML files from Alexandria folder. No sorting yet

--- app/controllers/Index.rb
+++ app/controllers/Index.rb
@@ -1,34 +1,7 @@
 class Index
 
-	def get_books
-		require 'models/Book'
-		Model_Book.get_all
-	end
-
 	def home
-		puts 'aushduashudasasasasahsa'
-		get_books
-
-		{
-			:recent => [
-				{:isbn => 56494121, :title => 'Rangers 4'},
-				{:isbn => 56494121, :title => 'Sherlock Holmes e o Jogador Perdido'}
-			],
-			:wishlist => [
-				{:isbn => 56494121, :title => 'Apartamento 41'},
-				{:isbn => 56494121, :title => 'O terceiro travesseiro'},
-				{:isbn => 56494121, :title => 'Cidade dos Ossos'},
-				{:isbn => 56494121, :title => 'Estação Polar'},
-				{:isbn => 56494121, :title => 'O Senhor da Chuva'}
-			],
-			:read => [
-				{:isbn => 56494121, :title => 'Escaldado em água fria'},
-				{:isbn => 56494121, :title => 'Sherlock Holmes e o Jogador Perdido'},
-				{:isbn => 56494121, :title => 'Nunca desista dos seus sonhos'},
-				{:isbn => 56494121, :title => 'A Batalha do Apocalipse'}
-			]
-		}
+		require 'models/Book'
+		Model_Book.get_summary
 	end
 end
-
-Index.new.get_books
--- app/models/Book.rb
+++ app/models/Book.rb
@@ -1,14 +1,66 @@
 class Model_Book
 
-	def Model_Book.get_all
+	def Model_Book.get_books
 		libraries = Array.new
 		Dir.entries(settings.folder).each do |dir|
 			libraries << dir unless dir.chars.next == '.'
 		end
 
-		libraries.each do |file|
-			puts Dir.entries(FOLDER+lib) if File.extname(file) == '.yaml'
+		@@books = Array.new
+		libraries.each do |lib|
+			Dir.entries(File.join settings.folder, lib).each do |file|
+				file = File.join settings.folder, lib, file
+				if File.extname(file) == '.yaml'
+					data = YAML.load_file(file).ivars
+					data.merge!({
+							'mtime' => File.mtime(file),
+							'id' => data['saved_ident'],
+							'read' => data['redd'],
+							'read_when' => data['redd_when'],
+						})
+
+					@@books << data
+				end
+			end
 		end
+
+		@@books
+	end
+
+	def Model_Book.get_recent
+		get_books unless instance_variable_defined?(:@books)
+	end
+
+	def Model_Book.get_wishlist
+		get_books unless class_variable_defined? :@@books
+		return @@wishlist if class_variable_defined? :@@wishlist
+
+		@@wishlist = Array.new
+		@@books.each do |book|
+			@@wishlist << book if !book['own'] and book['want']
+		end
+
+		@@wishlist
+	end
+
+	def Model_Book.get_read
+		get_books unless class_variable_defined? :@@books
+		return @@read if class_variable_defined? :@@read
+
+		@@read = Array.new
+		@@books.each do |book|
+			@@read << book if book['read']
+		end
+
+		@@read
+	end
+
+	def Model_Book.get_summary
+		{
+			:all => get_books,
+			:wishlist => get_wishlist,
+			:read => get_read
+		}
 	end
 
 end
